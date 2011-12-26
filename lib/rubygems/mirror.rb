2139075465982abb02981fa7d7288252782b0efa
Jason Lee <huacnlee@gmail.com>
2011-12-26 11:17:10 +0800

    Fetch others spec file on mirror.

--- lib/rubygems/mirror.rb
+++ lib/rubygems/mirror.rb
@@ -6,9 +6,12 @@ class Gem::Mirror
   autoload :Pool, 'rubygems/mirror/pool'
 
   VERSION = '1.0.1'
-
+  
   SPECS_FILE = "specs.#{Gem.marshal_version}"
   SPECS_FILE_Z = "specs.#{Gem.marshal_version}.gz"
+  SPECS_FILES = ["latest_specs.#{Gem.marshal_version}","latest_specs.#{Gem.marshal_version}.gz","Marshal.#{Gem.marshal_version}",
+                "Marshal.#{Gem.marshal_version}.Z","prerelease_specs.#{Gem.marshal_version}","prerelease_specs.#{Gem.marshal_version}.gz",
+                 "specs.#{Gem.marshal_version}","specs.#{Gem.marshal_version}.gz"]
 
   DEFAULT_URI = 'http://production.cf.rubygems.org/'
   DEFAULT_TO = File.join(Gem.user_home, '.gem', 'mirror')
@@ -33,6 +36,14 @@ class Gem::Mirror
     specz = to(SPECS_FILE_Z)
     @fetcher.fetch(from(SPECS_FILE_Z), specz)
     open(to(SPECS_FILE), 'wb') { |f| f << Gem.gunzip(File.read(specz)) }
+  end
+  
+  def fetch_all_specs
+    SPECS_FILES.each do |spec_file|
+      print "Fetching #{spec_file}..."
+      @fetcher.fetch(from(spec_file), to(spec_file))
+      puts "[Done]"
+    end
   end
 
   def gems
@@ -59,12 +70,17 @@ class Gem::Mirror
   end
 
   def update_gems
+    fetch_all_specs
+    puts ""
+    puts "-"*100
+    print "#{gems_to_fetch.count} Gems need to update"
     gems_to_fetch.each do |g|
       @pool.job do
         @fetcher.fetch(from('gems', g), to('gems', g))
         # download gemspec.rz
         g_spec = "#{g}spec.rz"
         @fetcher.fetch(from("quick/Marshal.#{Gem.marshal_version}", g_spec), to("quick/Marshal.#{Gem.marshal_version}", g_spec))
+        print "."
         yield if block_given?
       end
     end
