89c00094eb067b953b1288bc4568ea645271ab8c
Lamont Lucas <lamont@fastrobot.com>
2013-01-04 21:41:14 -0800

    look for missing *spec.rz files in the same fashion we look for missing .gem files

--- lib/rubygems/mirror.rb
+++ lib/rubygems/mirror.rb
@@ -69,8 +69,17 @@ class Gem::Mirror
     Dir[to('gems', '*.gem')].entries.map { |f| File.basename(f) }
   end
 
+  def existing_gemspecs
+    Dir[to("quick/Marshal.#{Gem.marshal_version}", '*.rz')].entries.map { |f| File.basename(f) }
+
+  end
+
   def gems_to_fetch
     gems - existing_gems
+  end
+
+  def gemspecs_to_fetch
+    gems.map {|g| "#{g}spec.rz" } - existing_gemspecs
   end
 
   def gems_to_delete
@@ -85,14 +94,17 @@ class Gem::Mirror
     gems_to_fetch.each do |g|
       @pool.job do
         @fetcher.fetch(from('gems', g), to('gems', g))
-        # download gemspec.rz
-        g_spec = "#{g}spec.rz"
+        print "."
+        yield if block_given?
+      end
+    end
+    gemspecs_to_fetch.each do |g_spec|
+      @pool.job do
         @fetcher.fetch(from("quick/Marshal.#{Gem.marshal_version}", g_spec), to("quick/Marshal.#{Gem.marshal_version}", g_spec))
         print "."
         yield if block_given?
       end
     end
-
     @pool.run_til_done
   end
 
