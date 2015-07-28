2752159e24dc3304fe34b0ca270fd0f8484dcf9b
James Tucker <jftucker@gmail.com>
2015-07-28 12:04:36 -0700

    Update version

--- lib/rubygems/mirror.rb
+++ lib/rubygems/mirror.rb
@@ -5,7 +5,7 @@ class Gem::Mirror
   autoload :Fetcher, 'rubygems/mirror/fetcher'
   autoload :Pool, 'rubygems/mirror/pool'
 
-  VERSION = '1.0.1'
+  VERSION = '1.1.0'
 
   SPECS_FILES = [ "specs.#{Gem.marshal_version}", "prerelease_specs.#{Gem.marshal_version}", "latest_specs.#{Gem.marshal_version}" ]
 
